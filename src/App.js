1d09bc16dc9d611525acfb028086608e7b75317e
Nguyen Cong Duy <ncongduy@gmail.com>
2021-05-26 20:40:37 +0300

    create alarm app using react hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+
+  const runTimer = () => {
+    const intervalid = setInterval(() => {
+      setTime((prev) => {
+        if (prev > 0) {
+          return (prev * 60 - 1) / 60;
+        } else {
+          clearInterval(intervalid);
+          window.open("https://youtu.be/kaf8PRNobYI", "_blank");
+          window.location.reload();
+          return 0;
+        }
+      });
+    }, 1000);
+  };
+
+  const handleChange = ({ target }) => {
+    setTime(target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="number" placeholder="Type minutes" onChange={handleChange} />
+      <button onClick={runTimer}>Run</button>
+      <p>{`${Math.floor((time * 60) / 60)}:${Math.floor((time * 60) % 60)}`}</p>
     </div>
   );
 }
