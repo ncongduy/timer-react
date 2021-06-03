2eaba5b0bc5927603f52bf8ce86ca13ee972280b
Nguyen Cong Duy <ncongduy@gmail.com>
2021-06-03 10:49:46 +0300

    fix bug: disable button after first click

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import React, { useState } from "react";
-import useSound from "use-sound";
+import useSound from "use-sound"; //npm install use-sound
 import music from "./audio/nothing mix adc _01.mp3";
 
 function App() {
   const [time, setTime] = useState(0);
+  const [disabled, setDisabled] = useState(false);
   const [play] = useSound(music);
 
   const runTimer = () => {
@@ -19,6 +20,7 @@ function App() {
         }
       });
     }, 1000);
+    setDisabled(true);
   };
 
   const resetApp = () => {
@@ -32,7 +34,9 @@ function App() {
   return (
     <div className="App">
       <input type="number" placeholder="Type minutes" onChange={handleChange} />
-      <button onClick={runTimer}>Run</button>
+      <button onClick={runTimer} disabled={disabled}>
+        Run
+      </button>
       <p>{`${Math.floor((time * 60) / 60)}:${Math.floor((time * 60) % 60)}`}</p>
       <button onClick={resetApp}>Reset</button>
     </div>
