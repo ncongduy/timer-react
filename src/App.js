670c0f8a1c1ca5649a953a8142e51209606c8cf7
Nguyen Cong Duy <ncongduy@gmail.com>
2021-06-03 22:56:05 +0300

    refactor: using useEffect in React Hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useSound from "use-sound"; //npm install use-sound
 import music from "./audio/nothing mix adc _01.mp3";
 
 function App() {
   const [time, setTime] = useState(0);
-  const [disabled, setDisabled] = useState(false);
+  const [playTimer, setPlayTimer] = useState(false);
   const [play] = useSound(music);
 
+  let minutes = Math.floor((time * 60) / 60);
+  let seconds = Math.floor((time * 60) % 60);
+
+  useEffect(() => {
+    if (playTimer) {
+      const intervalid = setInterval(() => {
+        setTime((prev) => {
+          if (prev > 0) {
+            return (prev * 60 - 1) / 60;
+          } else {
+            clearInterval(intervalid);
+            play();
+            return 0;
+          }
+        });
+      }, 1000);
+    }
+  }, [playTimer]);
+
   const runTimer = () => {
-    const intervalid = setInterval(() => {
-      setTime((prev) => {
-        if (prev > 0) {
-          return (prev * 60 - 1) / 60;
-        } else {
-          clearInterval(intervalid);
-          play();
-          return 0;
-        }
-      });
-    }, 1000);
-    setDisabled(true);
+    setPlayTimer(true);
   };
 
   const resetApp = () => {
@@ -34,10 +42,8 @@ function App() {
   return (
     <div className="App">
       <input type="number" placeholder="Type minutes" onChange={handleChange} />
-      <button onClick={runTimer} disabled={disabled}>
-        Run
-      </button>
-      <p>{`${Math.floor((time * 60) / 60)}:${Math.floor((time * 60) % 60)}`}</p>
+      <button onClick={runTimer}>Run</button>
+      <p>{`${minutes}:${seconds}`}</p>
       <button onClick={resetApp}>Reset</button>
     </div>
   );
