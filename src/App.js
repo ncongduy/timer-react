902b060e7dd0c149d0d789412215b47a7be5f6f9
Nguyen Cong Duy <ncongduy@gmail.com>
2021-06-15 22:55:04 +0300

    fix bug: delay timer - using date object

--- src/App.js
+++ src/App.js
@@ -1,47 +1,69 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { useState, useRef } from "react";
 import useSound from "use-sound"; //npm install use-sound
 import music from "./audio/nothing mix adc _01.mp3";
 
 function App() {
   const [time, setTime] = useState(0);
-  const [playTimer, setPlayTimer] = useState(false);
-  const [play] = useSound(music);
-
-  let minutes = Math.floor((time * 60) / 60);
-  let seconds = Math.floor((time * 60) % 60);
-
-  useEffect(() => {
-    if (playTimer) {
-      const intervalid = setInterval(() => {
-        setTime((prev) => {
-          if (prev > 0) {
-            return (prev * 60 - 1) / 60;
-          } else {
-            clearInterval(intervalid);
-            play();
-            return 0;
-          }
-        });
-      }, 1000);
-    }
-  }, [playTimer]);
-
-  const runTimer = () => {
-    setPlayTimer(true);
-  };
-
-  const resetApp = () => {
-    window.location.reload();
-  };
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isOngoing, setIsOnGoing] = useState(false);
+  const [play, { stop }] = useSound(music);
+  const runInterval = useRef(null);
 
   const handleChange = ({ target }) => {
     setTime(target.value);
   };
 
+  const checkTimer = (time) => {
+    if (time < 0) {
+      clearInterval(runInterval.current);
+      runInterval.current = null;
+      setTime(0);
+      setMinutes(0);
+      setSeconds(0);
+      setIsOnGoing(false);
+      play();
+      document.getElementById("inputNumber").value = 0;
+    }
+  };
+
+  const runTimer = () => {
+    if (isOngoing) return;
+    const startTimer = new Date().getTime();
+    const endTimer = startTimer + time * 60 * 1000;
+
+    runInterval.current = setInterval(() => {
+      setIsOnGoing(true);
+      let currentTimer = new Date().getTime();
+      let countTimer = endTimer - currentTimer;
+      setMinutes(() => Math.floor(countTimer / 60000) % 60);
+      setSeconds(() => Math.floor(countTimer / 1000) % 60);
+
+      //if on time, play music
+      checkTimer(countTimer);
+    }, 1000);
+  };
+
+  const resetApp = () => {
+    clearInterval(runInterval.current);
+    runInterval.current = null;
+    setTime(0);
+    setMinutes(0);
+    setSeconds(0);
+    setIsOnGoing(false);
+    stop();
+    document.getElementById("inputNumber").value = 0;
+  };
+
   return (
     <div className="App">
-      <input type="number" placeholder="Type minutes" onChange={handleChange} />
+      <input
+        id="inputNumber"
+        type="number"
+        placeholder="Type minutes"
+        onChange={handleChange}
+      />
       <button onClick={runTimer}>Run</button>
       <p>{`${minutes}:${seconds}`}</p>
       <button onClick={resetApp}>Reset</button>
