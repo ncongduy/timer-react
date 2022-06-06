b1642ed9c53aa90f537271323ab98a4c23df52ac
Nguyen Cong Duy <ncongduy@gmail.com>
2022-06-06 16:55:38 +0300

    feat: create a new input for task and refactor code

--- src/App.js
+++ src/App.js
@@ -1,76 +1,102 @@
-import "./App.css";
-import React, { useState, useRef, useEffect } from "react";
-import useSound from "use-sound"; //npm install use-sound
-import music from "./audio/nothing mix adc _01.mp3";
+import {useState, useRef, useEffect} from 'react';
+import useSound from 'use-sound';
+
+import music from './audio/nothing mix adc _01.mp3';
+import './App.css';
 
 function App() {
-  const [time, setTime] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const [task, setTask] = useState('');
+  const [time, setTime] = useState('');
+  const [hours, setHours] = useState('00');
+  const [minutes, setMinutes] = useState('00');
+  const [seconds, setSeconds] = useState('00');
   const [isOngoing, setIsOnGoing] = useState(false);
-  const [play, { stop }] = useSound(music);
+  const [play, {stop}] = useSound(music);
   const runInterval = useRef(null);
 
   useEffect(() => {
-    if (isOngoing) {
-      const startTimer = new Date().getTime();
-      const endTimer = startTimer + time * 60 * 1000 + 1000;
+    if (!isOngoing) return;
 
-      runInterval.current = setInterval(() => {
-        let currentTimer = new Date().getTime();
-        let countTimer = endTimer - currentTimer;
-        setMinutes(() => Math.floor(countTimer / 60000) % 60);
-        setSeconds(() => Math.floor(countTimer / 1000) % 60);
+    const startTimer = new Date().getTime();
+    const endTimer = startTimer + time * 60 * 1000 + 1000;
 
-        //if on time, play music
-        checkTimer(countTimer);
-      }, 1000);
-    }
+    runInterval.current = setInterval(() => {
+      let currentTimer = new Date().getTime();
+      let countTimer = endTimer - currentTimer;
+
+      setHours(() => `0${Math.floor((countTimer / (60 * 60 * 1000)) % 24)}`.slice(-2));
+      setMinutes(() => `0${Math.floor(countTimer / (60 * 1000)) % 60}`.slice(-2));
+      setSeconds(() => `0${Math.floor(countTimer / 1000) % 60}`.slice(-2));
+
+      //if on time, play music
+      checkTimer(countTimer);
+    }, 1000);
+
     return () => clearInterval(runInterval.current);
-  }, [isOngoing]);
+  }, [isOngoing, time]);
 
-  const handleChange = ({ target }) => {
-    setTime(target.value);
+  const resetTime = () => {
+    clearInterval(runInterval.current);
+    runInterval.current = null;
+    // setTask('');
+    // setTime('');
+    setHours('00');
+    setMinutes('00');
+    setSeconds('00');
+    setIsOnGoing(false);
   };
 
   const checkTimer = (time) => {
-    if (time < 0) {
-      clearInterval(runInterval.current);
-      runInterval.current = null;
-      setTime(0);
-      setMinutes(0);
-      setSeconds(0);
-      setIsOnGoing(false);
-      play();
-      document.getElementById("inputNumber").value = 0;
-    }
+    if (time >= 0) return;
+
+    resetTime();
+    play();
   };
 
-  const runTimer = () => {
+  const runTimer = (evt) => {
+    evt.preventDefault();
+
+    console.log('run timer');
     setIsOnGoing(true);
   };
 
+  const handleTaskInput = ({target}) => {
+    setTask(target.value);
+  };
+
+  const handleTimeInput = ({target}) => {
+    setTime(parseInt(target.value));
+  };
+
   const resetApp = () => {
-    clearInterval(runInterval.current);
-    runInterval.current = null;
-    setTime(0);
-    setMinutes(0);
-    setSeconds(0);
-    setIsOnGoing(false);
+    resetTime();
     stop();
-    document.getElementById("inputNumber").value = 0;
   };
 
   return (
     <div className="App">
-      <input
-        id="inputNumber"
-        type="number"
-        placeholder="Type minutes"
-        onChange={handleChange}
-      />
-      <button onClick={runTimer}>Run</button>
-      <p>{`${minutes}:${seconds}`}</p>
+      <form onSubmit={runTimer}>
+        <input
+          name="task"
+          type="text"
+          placeholder="Type your task"
+          value={task}
+          onChange={handleTaskInput}
+        />
+        <input
+          name="minutes"
+          type="text"
+          placeholder="Type minutes"
+          value={time}
+          onChange={handleTimeInput}
+        />
+        <input className="submit-btn" type="submit" value="Run" />
+
+        <p>
+          Task: {task} || {`${hours}:${minutes}:${seconds}`}
+        </p>
+      </form>
+
       <button onClick={resetApp}>Reset</button>
     </div>
   );
